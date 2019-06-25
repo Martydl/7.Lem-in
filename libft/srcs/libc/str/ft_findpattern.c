c592f5b3501524ddaac2e6604a034b7293b908a3
Martin DE LAGARDE <mde-laga@e3r12p19.42.fr>
2019-06-25 18:38:14 +0200

    need str in pattern

--- libft/srcs/libc/str/ft_findpattern.c
+++ libft/srcs/libc/str/ft_findpattern.c
@@ -6,21 +6,39 @@
 /*   By: mde-laga <mde-laga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 11:15:09 by mde-laga          #+#    #+#             */
-/*   Updated: 2019/06/25 13:51:10 by mde-laga         ###   ########.fr       */
+/*   Updated: 2019/06/25 18:32:38 by mde-laga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		string(char ***str)
+int		string(char ***str, char sep)
 {
-	while (*str)
-	{
-
-	}
+	while (**str == sep)
+		*str++;
+	while (**str != sep)
+		;
 }
 
-int		ft_findpattern(char *pattern, char *str)
+int		number(char ***str, char sep)
+{
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	while (**str == sep)
+		*str++;
+	while (**str != sep)
+		i++;
+	tmp = ft_strsub(*str, 0, i);
+	*str += i;
+	if (!ft_isint(tmp))
+		i = -1;
+	free(tmp);
+	return (i < 0 ? 0 : 1);
+}
+
+int		ft_findpattern(char *pattern, char *str, char sep)
 {
 	int		i;
 
@@ -28,18 +46,22 @@ int		ft_findpattern(char *pattern, char *str)
 	while (pattern[++i])
 	{
 		if (pattern[i] == '%')
+		{
 			if (pattern[i + 1] == 's' && i++)
-				if (!string())
+			{
+				if (!string(&str, sep))
 					return (0);
-			else if (pattern[i+ 1] == 'd' && i++)
-				if (!number())
+			}
+			else if (pattern[i + 1] == 'd' && i++)
+			{
+				if (!number(&str, sep))
 					return (0);
+			}
 			else
 				return (0);
+		}
 		else
 			return (0);
 	}
 	return (1);
 }
-
-//%s, %d
