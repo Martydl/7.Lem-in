04f4d0ccabc466bbb5f6082cca1f692b910ae852
Martydl <mde-laga@student.42.fr>
2019-06-25 13:51:27 +0200

    Began pattern fct

--- libft/srcs/libc/str/ft_findpattern.c
+++ libft/srcs/libc/str/ft_findpattern.c
@@ -6,13 +6,40 @@
 /*   By: mde-laga <mde-laga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 11:15:09 by mde-laga          #+#    #+#             */
-/*   Updated: 2019/06/17 14:00:25 by mde-laga         ###   ########.fr       */
+/*   Updated: 2019/06/25 13:51:10 by mde-laga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_findpattern(char *str)
+int		string(char ***str)
 {
+	while (*str)
+	{
 
+	}
 }
+
+int		ft_findpattern(char *pattern, char *str)
+{
+	int		i;
+
+	i = -1;
+	while (pattern[++i])
+	{
+		if (pattern[i] == '%')
+			if (pattern[i + 1] == 's' && i++)
+				if (!string())
+					return (0);
+			else if (pattern[i+ 1] == 'd' && i++)
+				if (!number())
+					return (0);
+			else
+				return (0);
+		else
+			return (0);
+	}
+	return (1);
+}
+
+//%s, %d
