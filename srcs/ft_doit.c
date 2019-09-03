5f7a98b0d4aef7cb027b8b7af3688786f756209b
Martin DE LAGARDE <mde-laga@e1r12p3.42.fr>
2019-09-03 11:10:43 +0200

    sav change lane format

--- srcs/ft_doit.c
+++ srcs/ft_doit.c
@@ -6,7 +6,7 @@
 /*   By: mde-laga <mde-laga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/30 16:40:21 by mde-laga          #+#    #+#             */
-/*   Updated: 2019/08/31 07:19:34 by mde-laga         ###   ########.fr       */
+/*   Updated: 2019/09/03 10:51:27 by mde-laga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 char	**ft_initroom(t_rm *rm)
 {
 	char	**room;
-	int i;
+	int		i;
 
 	if (!(room = (char**)malloc(sizeof(char) * ft_nbroom(rm))))
 		return (NULL);
-		i = 0;
+	i = 0;
 	while (rm)
 	{
 		room[i] = ft_strdup(rm->data[0]);
